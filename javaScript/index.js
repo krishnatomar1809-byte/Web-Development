16b5ad5114090aa14a00d2ab02c459802181d5c6
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-05 12:01:13 +0530

    Added js basics

---
+++ javaScript/index.js
@@ -0,0 +1,29 @@
+// let pencilPrice= 10;
+// let erasorPrice=5;
+// console.log(`The Total price is ${pencilPrice + erasorPrice} Rupess`);
+
+
+// let age=23;
+// console.log(age>18);
+
+
+// let marks=53;
+
+// if(marks>80){
+//     console.log("you have A grade");
+// }else if(marks>70){
+//     console.log("grade B");
+// }else if(marks>60){
+//     console.log("grade C");
+// }else if(marks>50){
+//     console.log("Grade D");
+// }
+
+
+let age=14;
+
+if(age>=18){
+    console.log("you can vote")
+}else{
+    console.log("you can't vote")
+}
