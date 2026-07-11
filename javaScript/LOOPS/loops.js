fb40057b28e14d8c3a89914e2d9be175c0808ffc
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-12 00:23:50 +0530

    Added Loop concept

---
+++ javaScript/LOOPS/loops.js
@@ -0,0 +1,11 @@
+// let num=prompt("enter the number: ");
+// for(let i=1;i<=10;i++){
+//     console.log(num*i);
+    
+// }
+
+let num=prompt("enter the number: ");
+for(let i=num;i<=num*10;i=i+num){
+    console.log(i);
+    
+}
