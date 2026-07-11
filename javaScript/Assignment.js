ad063f03c5707e07870741b5a6cfa529db346930
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-11 22:28:18 +0530

    Added js Assignment

--- javaScript/Assignment.js
+++ javaScript/Assignment.js
@@ -68,3 +68,43 @@
 // }else{
 //     console.log("they didn't have same last digit");
 // }
+
+
+// Assigment question
+
+
+// Q1.Write a JavaScript program to get the first n elements of an array.[n can be any positive number].Forexample:for array [7,9,0,-2]and n=3 Print,[7,9,0]
+
+// let arr=[7,9,0,2];
+// let n=3;
+// let ans1=arr.slice(0,n);
+// console.log(ans1);
+
+
+
+//Q2
+// let ans2=arr.slice(-n);
+// console.log(ans2);
+
+//Q3
+
+// let str=prompt("enter the string");
+
+// if(str.length==0){
+//     console.log("String is empty");
+// }
+// else{
+//     console.log("string is not empty");
+// }
+
+
+
+//Q4
+
+let str="KriShNA TomAr";
+let idx=5;
+if(str[idx]==str[idx].toLowerCase()){
+    console.log("both are same");
+}else{
+    console.log("they are not");
+}
--- javaScript/String.js
+++ javaScript/String.js
@@ -1,9 +1,9 @@
-let str= "Random String";
+// let str= "Random String";
 
-let arr=["january","july","march","august"];
-arr.shift();
-arr.shift();
-arr.unshift("june");
-arr.unshift("july");
+// let arr=["january","july","march","august"];
+// arr.shift();
+// arr.shift();
+// arr.unshift("june");
+// arr.unshift("july");
 
 
