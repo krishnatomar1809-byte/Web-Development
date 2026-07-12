343cb49f1f8cb5f30c237b6a8c0f8c574fc6a800
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-12 15:40:05 +0530

    Added guess the movie logic

--- javaScript/LOOPS/loops.js
+++ javaScript/LOOPS/loops.js
@@ -4,8 +4,22 @@
     
 // }
 
-let num=prompt("enter the number: ");
-for(let i=num;i<=num*10;i=i+num){
-    console.log(i);
+// let num=prompt("enter the number: ");
+// for(let i=num;i<=num*10;i=i+num){
+//     console.log(i);
     
+// }
+
+let favmovie="avatar";
+let guess=prompt("Enter the movie");
+while((guess != favmovie) && (guess != "quit")){
+    
+    guess=prompt("you r wring . please try again");
+
+}
+
+if(guess==favmovie){
+    console.log("congrats");
+}else{
+    console.log("you quit");
 }
