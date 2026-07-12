c13b31502e4863541a839d53dd7e0fd0b82dde67
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-12 22:09:54 +0530

    Added loops concept

---
+++ javaScript/LOOPS/loops-with-array.js
@@ -0,0 +1,25 @@
+// let arr=["mango","grapes","banana","apple","papaya"];
+
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(i,arr[i]);
+// }
+
+
+//for-of-loops
+
+// let heroes=[["superman","batman","spiderman"],["ironman","captain america","wonder women"]];
+
+// for(list of heroes){
+//     console.log(list);
+// }
+
+
+// nested for-of-loops
+
+let heroes=[["superman","batman","spiderman"],["ironman","captain america","wonder women"]];
+
+for(list of heroes){
+    for(hero of list){
+        console.log(hero);
+    }
+}
