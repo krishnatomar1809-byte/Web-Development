45eeb795e4009a6f993c92941e56ae229c3dc06a
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-21 19:48:57 +0530

    Added Assignmnt question

--- javaScript/Assignment/Assignment.js
+++ javaScript/Assignment/Assignment.js
@@ -11,5 +11,49 @@ function unquieStr(str){
     return ans;
 }
 
-console.log(unquieStr(str));
+// console.log(unquieStr(str));
+
+//Q3
+
+let arr=["Australia","Germany","United State Of America"];
+let long="";
+
+function longCoun(arr,long){
+    for(let i=0;i<arr.length;i++){
+       if(arr[i].length>long.length){
+            long=arr[i];
+        }
+    }
+    return long;
+}
+
+// console.log(longCoun(arr,long));
+
+// Q4
+
+let str1="apnacollege";
+
+function vowCount(str1){
+    let count=0;
+    for(let i=0;i<str1.length;i++){
+        if(str1[i]=='a' || str1[i]=='e' || str1[i]=='i' || str1[i]=='o' || str1[i]=='u'){
+            count++;
+        }
+        
+    }
+    return count;
+}
+
+console.log(vowCount(str1));
+
+
+// Q5
+
+let n=100;
+
+function randNum(n){
+    return Math.floor(Math.random()*n)+1;
+}
+
+console.log(randNum(n));
 
