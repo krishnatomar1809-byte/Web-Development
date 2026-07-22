835918965faf5aef25ef6c10993ae13eec551bd0
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-23 00:28:09 +0530

    Added try-catch

---
+++ javaScript/FUNCTION/try-catch.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log("hello");
+try{
+    console.log(a);
+} catch{
+    console.log("it's a error , a is not defined");
+}
+
+
+console.log("hello1");
+console.log("hello1");
