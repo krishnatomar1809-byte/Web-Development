e903e89d69b1890a8fbf41ef084fbcf2f7e7c913
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-23 01:51:12 +0530

    Added arrow function

---
+++ javaScript/FUNCTION/Arror-function.js
@@ -0,0 +1,24 @@
+let sum=(a,b) => {
+    console.log((a+b));
+}
+
+let cube=(n)=>{
+    return n*n*n;
+}
+
+let mul=(a,b)=> (
+    a*b
+);
+
+console.log("hello");
+
+setTimeout(()=>{
+    console.log("our webpage");
+},4000);
+
+console.log("welcome to");
+
+
+let id= setInterval(()=>{
+    console.log("setinterval hai ye");
+},2000);
