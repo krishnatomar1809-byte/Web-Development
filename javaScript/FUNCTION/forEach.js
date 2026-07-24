12ca621e43e8225067ed7581374067bed0372a62
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-24 22:29:31 +0530

    forEach Function

---
+++ javaScript/FUNCTION/forEach.js
@@ -0,0 +1,14 @@
+let arr=[
+    {
+        name:"krishna",
+        age:22
+    },
+    {
+        name:"aviral",
+        age:23,
+    }
+];
+
+arr.forEach((stu)=>{
+    console.log(stu.name);
+});
