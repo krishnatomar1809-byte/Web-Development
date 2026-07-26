85c4cbfbf6f0130fc842c642d5b835c9e413f10e
Krishna Tomar <krishnatomar1809@gmail.com>
2026-07-26 15:47:29 +0530

    Added Desstruction

---
+++ javaScript/FUNCTION/Destructing.js
@@ -0,0 +1,16 @@
+// Destruction Object
+
+const Student={
+    name:"krishna",
+    age:22,
+    marks:78
+};
+
+const {name: user ,age: birthDate, city="Morena"}=Student;
+
+// Destruction Array
+
+const student1=["krishna","aviral","anshu","shyam"];
+
+let [winner,runnerup]=student1;     // student ka first element winner me stroy aur second runnerup me store hoga
+
