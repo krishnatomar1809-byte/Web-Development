43cbc539e86028d6cac2ba84b024a629e3d7b370
Krishna Tomar <krishnatomar1809@gmail.com>
2026-08-01 18:49:30 +0530

    Added Event bubbling

---
+++ javaScript/DOM/Event-Bubbling.js
@@ -0,0 +1,20 @@
+let div=document.querySelector("div");
+let ul=document.querySelector("ul");
+let lis=document.querySelectorAll("li");
+
+div.addEventListener("click",function(event){
+    event.stopPropagation();
+    console.log("Div was clicked");
+})
+
+ul.addEventListener("click",function(event){
+    event.stopPropagation();
+    console.log("ul was clicked");
+})
+
+for(list of lis){
+    list.addEventListener("click",function(event){
+        event.stopPropagation();
+        console.log("list is clicked");
+    })
+}
