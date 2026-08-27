f264ec1c6241942b72fc84ddb038a41cca54942b
Krishna Tomar <krishnatomar1809@gmail.com>
2026-08-27 23:58:37 +0530

    Node concept complete

---
+++ Backend/math.js
@@ -0,0 +1,3 @@
+export const sum=a+b;
+export const mul=a*b;
+
--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,24 @@
-let a=5;
+// let a=5;
 
-for(let i=0;i<a;i++){
-    console.log(`hello ${i}`);
-}
+// for(let i=0;i<a;i++){
+//     console.log(`hello ${i}`);
+// }
+
+
+// require concept
+
+// const fruits= require("./fruits");
+// console.log(fruits);
+
+
+
+var giveMeAJoke = require('give-me-a-joke');
+
+// To get a random dad joke
+giveMeAJoke.getRandomDadJoke (function(joke) {
+    console.log(joke);
+});
+
+import {sum , mul} from "./math.js"
+
+console.log(sum(1,2));
